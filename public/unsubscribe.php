c3d5043e4aea3e0ab7b1545223998d31dd465994
Utsav-Ladani <201901076@daiict.ac.in>
2023-01-23 00:21:22 +0530

    Create unsubscribe page

---
+++ public/unsubscribe.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once '../Includes/Unsubscribe.class.php';
+
+$error = "";
+$success = 0;
+
+$email = isset($_GET["email"]) ? $_GET["email"] : "";
+$token = isset($_GET["token"]) ? $_GET["token"] : "";
+
+$unsubscriber = new Unsubscriber($email, $token);
+$error = $unsubscriber->error;
+$success = $error=="";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify your email</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <?php
+        if($error) { 
+            echo '<div class="error">'.$error.'</div>';
+        } 
+        else if($success) { 
+            echo '<div class="success">You have successfully unsubscribe GitHub Timeline.</div>';
+        } 
+    ?>
+</body>
+</html>
